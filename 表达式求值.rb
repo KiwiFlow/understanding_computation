a164dfc7503df2c349d44ee7df9759d37762111e
graphMachine <wanglei430426@gmail.com>
2016-07-26 08:45:19 +0800

    Create 表达式求值.rb

---
+++ 表达式求值.rb
@@ -0,0 +1,58 @@
+require 'matrix'
+require 'set'
+op = Set.new(['+','-','*','/','(',')','#'])
+$op_hash = {'+'=>0,'-'=>1,'*'=>2,'/'=>3,'('=>4,')'=>5,'#'=>6}
+$op_compare=Matrix[[1,1,-1,-1,-1,1,1],[1,1,-1,-1,-1,1,1],
+[1,1,1,1,-1,1,-1],[1,1,1,1,-1,1,-1],[-1,-1,-1,-1,-1,0,-2],
+[1,1,1,1,-2,1,1],[-1,-1,-1,-1,-1,-2,0]]
+
+
+$sOPND = []
+$sOPTR = ['#']
+
+Caculator = Object.new
+def Caculator.EvaluateExpression(input,op)
+	i=0
+	c=input[i]
+	
+	while c!='#' or $sOPTR[-1]!='#'
+		if not op.include?(c)
+		    $sOPND.push(Integer(c))
+			i+=1
+			c=input[i]
+		else
+		
+			if $op_compare[$op_hash[$sOPTR[-1]],$op_hash[c]]==-1
+				$sOPTR.push(c)
+				i+=1
+				c=input[i]
+			elsif $op_compare[$op_hash[$sOPTR[-1]],$op_hash[c]]==0
+				$sOPTR.pop()
+				i+=1 
+				c=input[i]
+			elsif $op_compare[$op_hash[$sOPTR[-1]],$op_hash[c]]==1
+				theta = $sOPTR[-1]
+				$sOPTR.pop()
+				b = $sOPND[-1]
+				$sOPND.pop()
+				a = $sOPND[-1]
+				$sOPND.pop()
+				
+				if theta == '+'
+					$sOPND.push(a+b)
+				elsif theta == '-'
+					$sOPND.push(a-b)
+				elsif theta == '*'
+					$sOPND.push(a*b)
+				else
+					$sOPND.push(Integer(a/b))
+				end
+				
+			end
+		end
+	end
+	$sOPND[-1]
+end
+
+
+print  Caculator.EvaluateExpression("1-3*5+6+8#",op)
